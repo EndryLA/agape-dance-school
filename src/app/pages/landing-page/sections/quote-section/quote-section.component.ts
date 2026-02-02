a683bcaecd52e36498435cf4493ff4c18b36aae0
Endry Lundy <clansdoli@gmail.com>
2026-02-02 22:56:01 +0100

    .

---
+++ src/app/pages/landing-page/sections/quote-section/quote-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-section',
+  templateUrl: './quote-section.component.html',
+  styleUrl: './quote-section.component.scss',
+})
+export class QuoteSectionComponent {}
